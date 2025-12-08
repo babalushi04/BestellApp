5c3d47282589cec304e5e7cd9263ec5c637a3488
babalushi04 <xhini95@gmail.com>
2025-12-08 23:05:34 +0100

    Add menu items for dishes, sides, drinks, and desserts

---
+++ scripts/library.js
@@ -0,0 +1,117 @@
+let myDishes = [
+    {
+        "name":"Spaghetti Carbonara",
+        "price":"12.99",
+        "description":"Ein klassisches italienisches Nudelgericht mit Eiern, Käse, Pancetta und Pfeffer.",
+        "amount":"0"
+    }
+    {
+        "name":"Vegetarische Lasagne",
+        "price":"9.99",
+        "description":"Eine leckere Lasagne mit frischem Gemüse und Käse.",
+        "amount":"0"
+    }
+    {
+        "name":"Hamburger",
+        "price":"10.99",
+        "description":"Ein saftiger Hamburger mit frischem Salat, Tomate und Zwiebel.",
+        "amount":"0"
+    }
+    {
+        "name":"Chicken Burger",
+        "price":"11.99",
+        "description":"Ein würziger Chicken Burger mit knusprigem Hähnchenfilet und frischem Gemüse.",
+        "amount":"0"
+    }
+    {
+        "name":"Margherita Pizza",
+        "price":"10.99",
+        "description":"Eine einfache Pizza mit Tomatensauce, frischem Mozzarella und Basilikum.",
+        "amount":"0"
+    }
+    {
+        "name":"Salami Pizza",
+        "price":"11.99",
+        "description":"Eine herzhafte Pizza mit würziger Salami und Käse.",
+        "amount":"0"
+    }
+    {
+        "name":"Diavola Pizza",
+        "price":"12.99",
+        "description":"Eine scharfe Pizza mit Peperoni und Käse.",
+        "amount":"0"
+    }
+];
+let myBeilages = [
+    {
+        "name":"Pommes Frites",
+        "price":"3.50",
+        "description":"Knusprige Pommes Frites, perfekt als Beilage.",
+        "amount":"0"
+    },
+    {
+        "name":"Knoblauchbrot",
+        "price":"4.00",
+        "description":"Frisches Knoblauchbrot, perfekt als Beilage.",
+        "amount":"0"
+    }
+    {
+        "name":"Bauern Salat",
+        "price":"5.00",
+        "description":"Ein frischer gemischter Salat mit verschiedenen Gemüsesorten.",
+        "amount":"0"
+    }
+];
+let myDrinks = [
+    {
+        "name":"Cola",
+        "price":"2.50",
+        "description":"Erfrischendes Cola-Getränk.",
+        "amount":"0"
+    },
+    {
+        "name":"Fanta",
+        "price":"2.50",
+        "description":"Fruchtiges Orangengetränk.",
+        "amount":"0"
+    },
+    {
+        "name":"Sprite",
+        "price":"2.50",
+        "description":"Erfrischendes Zitronen-Limetten-Getränk.",
+        "amount":"0"
+    },
+    {
+        "name":"Mineralwasser",
+        "price":"1.50",
+        "description":"Erfrischendes Mineralwasser.",
+        "amount":"0"
+    }
+    {
+        "name":"Apfelsaft",
+        "price":"2.00",
+        "description":"Frischer Apfelsaft.",
+        "amount":"0"
+    }
+];
+
+let myDesserts = [
+    {
+        "name":"Tiramisu",
+        "price":"4.50",
+        "description":"Ein klassisches italienisches Dessert mit Kaffee und Mascarpone.",
+        "amount":"0"
+    },
+    {
+        "name":"Panna Cotta",
+        "price":"4.00", 
+        "description":"Ein cremiges Dessert aus Sahne, Zucker und Gelatine.",
+        "amount":"0"
+    },
+    {
+        "name":"Schokoladenkuchen",
+        "price":"5.00",
+        "description":"Ein saftiger Schokoladenkuchen mit reichhaltiger Schokoladenglasur.",
+        "amount":"0"
+    }
+];
